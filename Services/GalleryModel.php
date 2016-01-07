90b7567e40b039f4d5529523064ff590eaeb2c78
Can Berkol <can.berkol@biberltd.com>
2016-01-07 12:16:06 +0200

    =Hotfix on getGalleryMedia()

--- Services/GalleryModel.php
+++ Services/GalleryModel.php
@@ -624,7 +624,7 @@ class GalleryModel extends CoreModel {
 		$file = $response->result->set;
 		$response = $this->getGallery($gallery);
 		if($response->error->exist){
-			return $response->result->set;
+			return $response;
 		}
 		$gallery = $response->result->set;
 		unset($response);
